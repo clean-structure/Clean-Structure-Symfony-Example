029e27a02af259a1698b5f209c74bc3675fb5e4c
Венделев Артём <artiom@vendelev.ru>
2025-08-26 21:00:21 +0300

    Add test for Welcome page

---
+++ backend/tests/ModuleSuite/WelcomePage/WelcomeControllerTest.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CleanStructure\Tests\ModuleSuite\WelcomePage;
+
+use CleanStructure\WelcomePage\Presentation\Http\Controller\WelcomeController;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+final class WelcomeControllerTest extends WebTestCase
+{
+    /**
+     * Проверим генерацию шаблона
+     *
+     * @see WelcomeController::page
+     */
+    public function testWelcomePage(): void
+    {
+        $client = self::createClient();
+
+        $client->request('GET', '/welcome');
+
+        self::assertResponseIsSuccessful();
+        self::assertSelectorTextContains('h1', 'Welcome to Symfony');
+    }
+}
