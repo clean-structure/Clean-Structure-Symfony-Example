988711505eeb3ea09bed46e69513aac3d2998efb
Венделев Артём <artiom@vendelev.ru>
2025-08-27 10:33:45 +0300

    Test optimisation

--- backend/tests/ModuleSuite/bootstrap.php
+++ backend/tests/ModuleSuite/bootstrap.php
@@ -3,12 +3,15 @@
 declare(strict_types=1);
 
 use Symfony\Component\Dotenv\Dotenv;
+use Symfony\Component\Filesystem\Filesystem;
 
 require dirname(__DIR__) . '/../vendor/autoload.php';
 
 if (method_exists(Dotenv::class, 'bootEnv')) { // @phpstan-ignore-line
     new Dotenv()->bootEnv(dirname(__DIR__) . '/../.env');
 }
+
+new Filesystem()->remove([__DIR__ . '/../../var/cache/test']);
 
 if ($_SERVER['APP_DEBUG']) { // @phpstan-ignore-line
     umask(0000);
